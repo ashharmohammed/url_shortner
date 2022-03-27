2e6ee35e8e3638eeb584c6ff1de6e9c56b85d247
ashhar-mohammed <Ashhar.mohammed@netbraintech.com>
2022-03-27 12:59:07 +0530

    correcting posgres sql url

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
 from decouple import config
 
+
+DATABASE_URI = config("DATABASE_URL")
+if DATABASE_URI.startswith("postgres://"):
+    DATABASE_URI = DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
 class Config(object):
     DEBUG = False
     TESTING = False
     SECRET_KEY = config('SECRET_KEY', default='sample_key')
-    SQLALCHEMY_DATABASE_URI = config('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = DATABASE_URI
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
